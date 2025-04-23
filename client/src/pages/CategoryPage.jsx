c4b4d74cc3ea7ade4722c06873d8fad8a99477f6
Amal-Elafifi <160831057+Amal-Elafifi@users.noreply.github.com>
2025-04-23 08:20:19 -1100

    Update CategoryPage.jsx

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -5,7 +5,7 @@ const CategoryPage = () => {
   const { categoryName } = useParams();
 
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center p-10">
+    <div className="min-h-screen mt-10 bg-white flex flex-col items-center justify-center p-10">
       <h1 className="text-3xl font-bold mb-4 capitalize">{categoryName} Page</h1>
       <p className="text-gray-600 mb-6">Here you'll see all blog posts or content related to <strong>{categoryName}</strong>.</p>
       <Link to="/" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">← Back to Home</Link>
