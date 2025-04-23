c371465d0255afadcc671dd58e245177b6d23e51
Amal-Elafifi <160831057+Amal-Elafifi@users.noreply.github.com>
2025-04-23 08:40:29 -1100

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -48,10 +48,10 @@ function Contact () {
                     <Formik>
                     {({ isSubmitting }) => (
                         <Form className="flex flex-col p-5">
-                            <Field className={inputClassName} type="text" name="name" placeholder="name" />
-                            <Field className={inputClassName} type="address" name="city" placeholder="city"/>
-                            <Field className={inputClassName} type="email" name="email" placeholder="email" />
-                            <Field className={`${inputClassName} pt-5 pb-20`} placeholder="message"/>
+                            <Field className={inputClassName} required type="text" name="name" placeholder="name" />
+                            <Field className={inputClassName} required type="address" name="city" placeholder="city"/>
+                            <Field className={inputClassName} required type="email" name="email" placeholder="email" />
+                            <Field className={`${inputClassName} pt-5 pb-20`} required placeholder="message"/>
                             <button type="submit" disabled={isSubmitting} className="ease-in-out bg-black hover:bg-white hover:text-black hover:border-2 hover:border-black w-[150px] font-medium text-[20px] text-center py-4 text-white rounded-xl">
                                 Submit
                             </button>
